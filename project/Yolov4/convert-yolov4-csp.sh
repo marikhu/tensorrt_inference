964c1725d50e244d7f411b8175d5a105e190a82f
Ramesh Marikhu <marikhu@gmail.com>
2023-11-10 10:46:57 +0545

    Creating onnx file for YOLOv4-CSP -- using batch size of 6

---
+++ project/Yolov4/convert-yolov4-csp.sh
@@ -0,0 +1,1 @@
+python3 export_onnx.py --cfg_file 14-YOLOv4-CSP-2Classes-BMA-v1/yolov4-csp-2class_test.cfg --weights_file 14-YOLOv4-CSP-2Classes-BMA-v1/yolov4-csp-2class_best.weights --output_file yolov4-csp.onnx
